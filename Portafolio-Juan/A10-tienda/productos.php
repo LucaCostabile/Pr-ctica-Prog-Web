be80f1f03ae9320e8b723b37292f01789f1b47ff
Juan95Kruz <70464916+Juan95Kruz@users.noreply.github.com>
2025-09-29 18:45:35 -0300

    Update productos.php

--- Portafolio-Juan/A10-tienda/productos.php
+++ Portafolio-Juan/A10-tienda/productos.php
@@ -1,7 +1,8 @@
-<?php
-// Fuente de datos simple de productos (podrías pasarlo a BD si querés)
+ir<?php
+// Fuente de datos simple de productos
 return [
   ['id' => 1, 'nombre' => 'Producto A', 'precio' => 10],
   ['id' => 2, 'nombre' => 'Producto B', 'precio' => 20],
   ['id' => 3, 'nombre' => 'Producto C', 'precio' => 30],
 ];
+
